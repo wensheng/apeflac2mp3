1c8531dc3cba296dfbd1d42309c102ad1283c1a8
njonesu <niklas.jq@outlook.com>
2019-01-06 14:50:14 +0000

    Feats: Copy dirtree, concurrency + minor changes
    The subdirectories of the input dir are copied to the outdir unless the -s flag is passed. ProcessPoolExecutor is used for concurrency but can be disabled in the config.ini. Other minor changes were also made.

--- apeflac2mp3.py
+++ apeflac2mp3.py
@@ -4,10 +4,12 @@ import sys
 import os
 import platform
 import argparse
+from multiprocessing import cpu_count
 from configparser import ConfigParser
+from concurrent.futures import ProcessPoolExecutor
 
 if sys.version_info[0] != 3:
-    exit("Only Python 3 supported")
+    sys.exit("Only Python 3 is supported.")
 system = platform.system()
 if system == 'Windows':
     sname = 'win'
@@ -16,15 +18,19 @@ elif system == 'Darwin':
 elif system == 'Linux':
     sname = 'linux'
 else:
-    exit("Unknown sytem, can't continue")
+    sys.exit("Unknown sytem, can't continue.")
 
 cfg = ConfigParser()
 cfg.read(os.path.join(os.path.realpath(os.path.dirname(__file__)), 'config.ini'))
 
-ffmpeg_path = cfg.get(sname, "ffmpeg")
-supported_exts = ("flac", "ape", "m4a", "oga")
+ffmpeg_path = cfg.get(sname, 'ffmpeg')
+supported_exts = cfg.get('options', 'supported_exts').split(',')
+use_multiprocessing = cfg.getboolean('options', 'multiprocessing')
+# Ask for confirmation before process_dir()
+prompt_to_continue = cfg.get('options', 'prompt_to_continue')
 # ffmpeg args
 bitrate = cfg.get('options', 'bitrate')
+nostats = cfg.getboolean('options', 'nostats')
 
 
 def parse_cue(cue_file, outdir):
@@ -35,11 +41,11 @@ def parse_cue(cue_file, outdir):
     """
     cue_dir = os.path.dirname(cue_file)
     d = open(cue_file).read().splitlines()
-    
+
     general = {}
     tracks = []
     current_file = None
-    
+
     for line in d:
         if line.startswith('REM GENRE '):
             general['genre'] = ' '.join(line.split(' ')[2:]).replace('"', '')
@@ -51,13 +57,13 @@ def parse_cue(cue_file, outdir):
             general['album'] = ' '.join(line.split(' ')[1:]).replace('"', '')
         if line.startswith('FILE '):
             current_file = ' '.join(line.split(' ')[1:-1]).replace('"', '')
-        
+
         if line.startswith('  TRACK '):
             track = general.copy()
             track['track'] = int(line.strip().split(' ')[1], 10)
-    
+
             tracks.append(track)
-    
+
         if line.startswith('    TITLE '):
             tracks[-1]['title'] = ' '.join(line.strip().split(' ')[1:]).replace('"', '')
         if line.startswith('    PERFORMER '):
@@ -66,11 +72,11 @@ def parse_cue(cue_file, outdir):
             #t = map(int, ' '.join(line.strip().split(' ')[2:]).replace('"', '').split(':'))
             t = [int(a) for a in ' '.join(line.strip().split(' ')[2:]).replace('"', '').split(':')]
             tracks[-1]['start'] = 60 * t[0] + t[1] + t[2] / 100.0
-    
+
     for i in range(len(tracks)):
         if i != len(tracks) - 1:
             tracks[i]['duration'] = tracks[i + 1]['start'] - tracks[i]['start']
-    
+
     cmds = []
     for track in tracks:
         metadata = {
@@ -84,21 +90,28 @@ def parse_cue(cue_file, outdir):
             metadata['genre'] = track['genre']
         if 'date' in track:
             metadata['date'] = track['date']
-    
+
         cmd = ffmpeg_path
-        cmd += ' -i "%s"' % os.path.join(cue_dir,current_file)
-        cmd += ' -ss %.2d:%.2d:%.2d' % (track['start'] / 60 / 60, track['start'] / 60 % 60, int(track['start'] % 60))
+        if nostats:
+            cmd += ' -nostats'
+        cmd += ' -i "%s"' % os.path.join(cue_dir, current_file)
+        cmd += ' -ss %.2d:%.2d:%.2d' % (int(track['start']) / 60 / 60,
+                                        int(track['start']) / 60 % 60,
+                                        int(track['start']) % 60)
 
         if 'duration' in track:
-            cmd += ' -t %.2d:%.2d:%.2d' % (track['duration'] / 60 / 60, track['duration'] / 60 % 60, int(track['duration'] % 60))
-
-        cmd += ' ' + ' '.join('-metadata %s="%s"' % (k, v) for (k, v) in metadata.items())
+            cmd += ' -t %.2d:%.2d:%.2d' % (int(track['duration']) / 60 / 60,
+                                           int(track['duration']) / 60 % 60,
+                                           int(track['duration']) % 60)
+        cmd += ' ' + ' '.join(
+            '-metadata %s="%s"' % (k, v) for (k, v) in metadata.items())
         cmd += ' -ab %s' % bitrate
-        filename = '%s-%s-%.2d.mp3' % (track['artist'].replace(":", "-"),
-                                       track['title'].replace(":", "-"),
-                                       track['track'])
+        cmd += ' ' + ' '.join('-metadata %s="%s"' % (k, v) for (k, v) in metadata.items())
+        filename = '%.2d - %s.mp3' % (int(track['track']),
+                                      # track['artist'].replace(":", "-"),
+                                      track['title'].replace(":", "-"))
         cmd += ' "%s"' % os.path.join(outdir, filename)
-    
+
         cmds.append(cmd)
 
     return cmds
@@ -106,9 +119,16 @@ def parse_cue(cue_file, outdir):
 
 def process_cue(incue, outdir):
     cmds = parse_cue(incue, outdir)
-    for cmd in cmds:
-        print(cmd)
-        os.system(cmd)
+    if use_multiprocessing:
+        with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
+            for cmd in cmds:
+                executor.submit(
+                    os.system,
+                    cmd
+                )
+    else:
+        for cmd in cmds:
+            os.system(cmd)
 
 
 def process_onefile(infile, outdir):
@@ -123,36 +143,109 @@ def process_onefile(infile, outdir):
     os.system(cmd)
 
 
-def process_dir(indir, outdir):
+def validate_path(path, isdir_only=False):
+    """
+    Exit if the path is not a valid file or directory.
+    """
+    if isdir_only:
+        if not os.path.isdir(path):
+            print('Error: Invalid directory ' + path)
+            sys.exit(1)
+    else:
+        if not os.path.isfile(path) and not os.path.isdir(path):
+            print('Error: Invalid path ' + path)
+            sys.exit(1)
+
+
+def try_mkdir(directory):
+    """
+    Try os.mkdir and exit if it fails.
+    """
+    try:
+        os.mkdir(directory)
+    except PermissionError as e:
+        print('%s is possibly an invalid directory.'
+              '\nError message: ' + e)
+        sys.exit(1)
+    except Exception as e:
+        print('Error while making %s: %s' % (directory, e))
+        sys.exit(1)
+
+
+def prompt_user(message):
+    """
+    Prompt user to continue the script or exit.
+    """
+    a = input('%s (y = yes) ' % str(message))
+    if a == 'y' or a == 'Y':
+        return
+    sys.exit()
+
+
+def process_dir(indir, outdir, same_dir=False):
     """
     Recursively go through directory.
     If it find cue files, it will process them.
-    Otherwise it will convert files of known formats (supported_exts) to mp3
+    Otherwise it will convert files of known formats (supported_exts) to mp3.
+
+    Creates subdirectories in the outdir unless same_dir is True.
+    The subdirectories will have the same directory tree as the indir's.
     """
+    basedir = os.path.basename(indir)
+
+    if not same_dir:
+        if not os.path.exists(outdir):
+            validate_path(outdir, True)
+            try_mkdir(outdir)
+        outbasedir = os.path.basename(outdir)
+        if basedir != outbasedir:
+            outdir = os.path.join(outdir, basedir)
+        if not os.path.exists(outdir):
+            try_mkdir(outdir)
+
+    if prompt_to_continue:
+        prompt_user('Output directory = %s\nContinue?' % outdir)
+    current_outdir = outdir
     for root, dirs, files in os.walk(indir):
-        cues = [f for f in files if f.endswith(".cue")]
+        if not same_dir:
+            if os.path.basename(root) not in outdir:
+                in_subtree = root.split(basedir)[1][1:]
+                current_outdir = os.path.join(outdir, in_subtree)
+            if not os.path.exists(current_outdir):
+                os.mkdir(current_outdir)
+
+        cues = [f for f in files if f.endswith('.cue')]
         if cues:
             for cue in cues:
-                process_cue(os.path.join(root, cue), outdir)
+                process_cue(os.path.join(root, cue), current_outdir)
         else:
             for f in files:
                 name, ext = os.path.splitext(f)
                 if ext in supported_exts:
-                    process_onefile(os.path.join(root, f), outdir)
+                    process_onefile(os.path.join(root, f), current_outdir)
 
 
-if "__main__" == __name__:
+if __name__ == '__main__':
     se = supported_exts
     se.insert(len(supported_exts) - 1, 'and')
     se = ', '.join(se).replace('and,', 'and')
     parser = argparse.ArgumentParser(
-        'apeflac2mp3.py'
+        'apeflac2mp3.py',
+        description='Splits .cue files and/or converts %s files to mp3. If '
+                    'the input argument is a directory the output will have '
+                    'the same subdirectory tree unless the -s flag is passed.'
+                    % se
     )
     parser.add_argument('input', help='the directory or file to process')
     parser.add_argument('outdir', nargs='?', help='the output directory')
     parser.add_argument('-b', '--bitrate', dest='bitrate',
                         help='The bitrate of the mp3 files. Default = 320k')
+    parser.add_argument('-s', '--samedir', dest='same_dir',
+                        action='store_true',
+                        help='put all output in one directory (outdir)')
     args = parser.parse_args()
+
+    validate_path(args.input)
 
     if args.bitrate:
         minb = 65
